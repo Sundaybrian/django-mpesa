afbd2c0b555c745f81cbb8a2a38ba30f2ba5abb0
Brian Sunday <brians931@gmail.com>
2020-09-09 14:24:53 +0300

    refactor lipa na mpesa online

--- src/mpesa/lnm.js
+++ src/mpesa/lnm.js
@@ -1,12 +1,32 @@
+const axios = require("axios");
+
 module.exports = async function (
   shortCode = null,
   passkey = null,
+  password,
   amount,
   transactionType = "CustomerPayBillOnline",
   senderMsisdn,
-  callbackUrl,
-  accountReference,
+  callbackUrl = null,
+  accountRef,
   transactionDesc
 ) {
-  const _shortCode = shortCode;
+  const formatted_time = new Date()
+    .toISOString()
+    .replace(/[^0-9]/g, "")
+    .slice(0, -3);
+
+  return req.post("mpesa/stkpush/v1/processrequest", {
+    BusinessShortCode: process.env["LIPA_NA_MPESA_SHORTCODE"],
+    Password: password,
+    Timestamp: formatted_time,
+    TransactionType: transactionType,
+    Amount: amount || "1",
+    PartyA: senderMsisdn || process.env["PHONE_NUMBER"],
+    PartyB: process.env["LIPA_NA_MPESA_SHORTCODE"],
+    PhoneNumber: senderMsisdn || process.env["PHONE_NUMBER"],
+    CallBackURL: process.env["CALLBACK_URL"],
+    AccountReference: accountRef || "123456780",
+    TransactionDesc: transactionDesc || "buymilk",
+  });
 };
